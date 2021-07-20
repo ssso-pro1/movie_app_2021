1c70e1eba49769f079f92042f30572347ff52e89
Soyeon Jang <ssso.pro1@gmail.com>
2021-07-20 22:40:49 +0900

    add class App component
    getMovies function, componentDidMount(), render()

--- src/App.js
+++ src/App.js
@@ -1,62 +1,61 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie.js';
+import './App.css';
 
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image:
-      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image:
-      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
-    rating: 4.7,
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
-    rating: 4.3,
-  },
-];
+class App extends React.Component {
+  state = {
+    //state
+    isLoading: true,
+    movies: [],
+  };
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I love {name}</h2>
-      <img src={picture} alt={name} />
-    </div>
-  );
+  getMovies = async () => {
+    //함수
+
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    // console.log(movies);
+    this.setState({ movies, isLoading: false });
+    //this.setState 하는 거 : state의 movies갖고오기 때문
+    // 이거 없으면 Loading... 넣으면 ready됨
+  };
+
+  componentDidMount() {
+    this.getMovies(); //this.
+  }
+
+  render() {
+    const { isLoading, movies } = this.state; //이부분 state
+    return (
+      // html처럼 보이지만 jsx이다.
+      //class 대신 className
+      <section className='container'>
+        {isLoading ? (
+          <div className='loader'>
+            <span className='loader__text'>'Loading...'</span>
+          </div>
+        ) : (
+          <div className='movies'>
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+      //return할때 {}
+    );
+  }
 }
-
-Food.propTypes = {};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
-      ))}
-    </div>
-  );
-}
-
 export default App;
